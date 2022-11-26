b9d23b8b0eaa71758d60db09233ec14c330c05c7
marmooo <marmooo@users.noreply.github.com>
2022-11-26 13:00:43 +0900

    bump bootstrap from 5.2.2 to 5.2.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-10-08 11:03";
+var CACHE_NAME = "2022-11-26 12:58";
 var urlsToCache = [
   "/talk-yomi/",
   "/talk-yomi/index.js",
@@ -6,7 +6,7 @@ var urlsToCache = [
   "/talk-yomi/mp3/incorrect1.mp3",
   "/talk-yomi/mp3/end.mp3",
   "/talk-yomi/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
